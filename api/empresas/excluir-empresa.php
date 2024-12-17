e642c3789d32592ab5e262f85e84e633411a0a6d
Erick Gabriel <erickgabrielalves@hotmail.com>
2024-12-17 13:59:56 -0300

    feat: crud empresa

--- api/empresas/excluir-empresa.php
+++ api/empresas/excluir-empresa.php
@@ -1,7 +1,29 @@
 <?php 
     session_start();
     include_once("../../database/conn.php");
-    include_once("../../includes/validacoes.php");
     include_once("../../includes/alerta.php");
 
+    $idEmpresa = isset($_POST["idEmpresa"]) ? $_POST["idEmpresa"] : "";
+
+    $sql = "DELETE FROM EMPRESAS
+            WHERE IDEMPRESA = :idEmpresa";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":idEmpresa", $idEmpresa);
+
+    try {
+        $stmt->execute();
+
+        redirecionar("Empresas", "Empresa excluída com Sucesso!", "green");
+        exit;
+    } catch (PDOException $ex) {
+        if ($ex->getCode() == "23000") {
+            if (strpos($ex->getMessage(), "a foreign key constraint fails") != false) {
+                redirecionar("Empresas", "Erro ao excluir a Empresa. Ela está relacionada a outros registros.", "red");
+            }
+        } else {
+            redirecionar("Empresas", "Erro ao excluir a Empresa. Erro: $ex", "red");
+            exit;
+        }
+    }
 ?>

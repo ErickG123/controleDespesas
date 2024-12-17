321ee8609e9f2009a467cfa5326ad5ee155c40f5
Erick Gabriel <erickgabrielalves@hotmail.com>
2024-12-17 14:00:31 -0300

    feat: crud equipamento

--- api/equipamentos/criar-equipamento.php
+++ api/equipamentos/criar-equipamento.php
@@ -1,7 +1,29 @@
 <?php 
     session_start();
     include_once("../../database/conn.php");
-    include_once("../../includes/validacoes.php");
     include_once("../../includes/alerta.php");
 
+    $equipamento = isset($_POST["equipamento"]) ? $_POST["equipamento"] : "";
+
+    $campos = [
+        "equipamento" => $equipamento
+    ];
+
+    validarCampos("Equipamentos", $campos);
+
+    $sql = "INSERT INTO EQUIPAMENTOS (EQUIPAMENTO)
+            VALUES (:equipamento)";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":equipamento", $equipamento);
+
+    try {
+        $stmt->execute();
+
+        redirecionar("Equipamentos", "Equipamento cadastrado com Sucesso", "green");
+        exit;
+    } catch (PDOException $ex) {
+        redirecionar("Equipamentos", "Erro ao cadastrar o Equipamento. Erro: $ex", "red");
+        exit;
+    }
 ?>
--- api/equipamentos/editar-equipamento.php
+++ api/equipamentos/editar-equipamento.php
@@ -1,7 +1,32 @@
 <?php 
     session_start();
     include_once("../../database/conn.php");
-    include_once("../../includes/validacoes.php");
     include_once("../../includes/alerta.php");
 
+    $idEquipamento = isset($_POST["idEquipamento"]) ? $_POST["idEquipamento"] : "";
+    $equipamento = isset($_POST["equipamento"]) ? $_POST["equipamento"] : "";
+
+    $campos = [
+        "equipamento" => $equipamento
+    ];
+
+    validarCampos("Equipamentos", $campos);
+
+    $sql = "UPDATE EQUIPAMENTOS SET
+            EQUIPAMENTO = :equipamento
+            WHERE IDEQUIPAMENTO = :idEquipamento";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":equipamento", $equipamento);
+    $stmt->bindParam(":idEquipamento", $idEquipamento);
+
+    try {
+        $stmt->execute();
+
+        redirecionar("Equipamentos", "Equipamento atualizado com Sucesso!", "green");
+        exit;
+    } catch (PDOException $ex) {
+        redirecionar("Equipamentos", "Erro ao atualizar o Equipamento.", "red");
+        exit;
+    }
 ?>

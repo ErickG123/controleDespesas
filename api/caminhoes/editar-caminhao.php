ba4901650f1bb21b0f32f720e9a7586c9c479df3
Erick Gabriel <erickgabrielalves@hotmail.com>
2024-12-17 13:59:33 -0300

    feat: crud caminhao

--- api/caminhoes/editar-caminhao.php
+++ api/caminhoes/editar-caminhao.php
@@ -4,4 +4,30 @@
     include_once("../../includes/validacoes.php");
     include_once("../../includes/alerta.php");
 
+    $idCaminhao = isset($_POST["idCaminhao"]) ? $_POST["idCaminhao"] : "";
+    $caminhao = isset($_POST["caminhao"]) ? $_POST["caminhao"] : "";
+
+    $campos = [
+        "caminhão" => $caminhao
+    ];
+
+    validarCampos("Caminhões", $campos);
+
+    $sql = "UPDATE CAMINHOES SET
+            CAMINHAO = :caminhao
+            WHERE IDCAMINHAO = :idCaminhao";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":caminhao", $caminhao);
+    $stmt->bindParam(":idCaminhao", $idCaminhao);
+
+    try {
+        $stmt->execute();
+
+        redirecionar("Caminhões", "Caminhão atualizado com Sucesso!", "green");
+        exit;
+    } catch (PDOException $ex) {
+        redirecionar("Caminhões", "Erro ao atualizar o Caminhão.", "red");
+        exit;
+    }
 ?>
